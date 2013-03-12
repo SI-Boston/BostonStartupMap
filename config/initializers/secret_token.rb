c51ae7ce3a949995e1fbcef869e2dd712a5b4ba8
Jamie Davidson <jhdavids8@gmail.com>
2013-03-12 15:02:02 -0400

    Initial commit of shell rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BostonStartupMap::Application.config.secret_token = '106fec71ad70785cc1966f39ab5129262267003e9f1fd79d4b6bd5e8d56e3c542b1c3b723866380b8812bbc60eff19d09454ad0f1e5fcc8ee5c3b15236827ff9'
