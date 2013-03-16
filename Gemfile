59adbce98067a69ab2c64a9d83ad62bc0ebb0448
Jamie Davidson <jhdavids8@gmail.com>
2013-03-16 16:13:42 -0400

    Added geocoder

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'jquery-rails'
 gem "devise"
 gem 'rabl'
 gem 'yajl-ruby'
+gem "geocoder"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,9 +2,15 @@ class Company < ActiveRecord::Base
   attr_accessible :name, :url, :description, :address, :zip, :latitude, :longitude, :image, :remote_image_url, :twitter_handle, :facebook_handle, :category
   validates_uniqueness_of :name
   mount_uploader :image, ImageUploader
+  geocoded_by :full_street_address
+  before_create :geocode    
 
   def category_enum
     # Do not select any value, or add any blank field. RailsAdmin will do it for you.
     ['Startup', 'Accelerator/Incubator', 'Investor', 'Co-working space', 'Hang-out spot']
+  end
+
+  def full_street_address
+    "#{address}, #{zip}"
   end
 end
